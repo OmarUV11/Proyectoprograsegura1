7cbeb95148eb72086a3f01c4d0b2885a6adbe8f1
ErikLunaVilla <eriklunavilla@gmail.com>
2022-06-17 23:40:24 -0500

    Actualizacion con sockets incompleto

--- ProyectoSegura/app/sistemaSeg/sistemaSeg/views.py
+++ ProyectoSegura/app/sistemaSeg/sistemaSeg/views.py
@@ -15,7 +15,48 @@ import string
 import random
 import re
 import hashlib
+#__
+#socket
+import socket
+import threading
+import sys
+#____
+import mensajes
 
+#____Cliente Socket
+
+def conectar_servidor(host, puerto):
+    # socket para IP v4
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        cliente.connect((host, int(puerto)))
+        return cliente
+    except:
+        print('Servidor inalcanzable')
+        exit()
+
+def leer_mensajes(cliente):
+    while True:
+        mensaje = mensajes.leer_mensaje(cliente)
+        print('-->' + mensaje.decode('utf-8'))
+
+
+def enviar_mensaje_loop(cliente):
+    mensaje = b''
+    while mensaje.strip() != b'exit':
+        mensaje = input('Mensaje:hola perro')
+        mensaje = mensaje.encode('utf-8')
+        mensajes.mandar_mensaje(cliente, mensaje)     
+
+      
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    puerto = 8002
+    cliente = conectar_servidor(host, puerto)
+    hilo = threading.Thread(target=leer_mensajes, args=(cliente,))
+    hilo.start()
+    enviar_mensaje_loop(cliente)
+#____Aqui termina lo del socket
 
 
 @login_requerido
